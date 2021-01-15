113f0df38544cfedc577798465ed2c1487b13776
Sandeep Bassi <thecodingfiles@gmail.com>
2021-01-15 22:22:58 +0000

    added redirect and uique id passed as query params

--- public/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-const getGif = () => {
-  $.ajax('https://cors-anywhere.herokuapp.com/https://www.affirmations.dev/')
-    .then( data => console.log('affirmation:', data.affirmation))
-    .catch( err => console.log('error getting gif', err));
-};
-getGif();
